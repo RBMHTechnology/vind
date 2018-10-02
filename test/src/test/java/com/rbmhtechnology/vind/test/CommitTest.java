93ed1ad7feed5987268f8c6d5a8d3759cd99bd51
Thomas Kurz <th.kurz@gmail.com>
2018-10-02 13:05:43 +0200

    #73 add some tests

--- test/src/test/java/com/rbmhtechnology/vind/test/CommitTest.java
+++ test/src/test/java/com/rbmhtechnology/vind/test/CommitTest.java
@@ -1,5 +1,7 @@
 package com.rbmhtechnology.vind.test;
 
+import com.google.common.collect.ImmutableList;
+import com.rbmhtechnology.vind.api.Document;
 import com.rbmhtechnology.vind.api.query.Search;
 import com.rbmhtechnology.vind.model.DocumentFactory;
 import com.rbmhtechnology.vind.model.DocumentFactoryBuilder;
@@ -24,6 +26,44 @@ public class CommitTest {
         Thread.sleep(1100);
 
         Assert.assertEquals(1, testSearchServer.getSearchServer().execute(Search.fulltext(), document).getNumOfResults());
+
+        testSearchServer.getSearchServer().clearIndex();
+    }
+
+    @Test
+    public void testMultipleDocumentsCommitWithin() throws InterruptedException {
+        testSearchServer.getSearchServer().indexWithin(ImmutableList.of(
+               document.createDoc("2"),
+               document.createDoc("3")
+        ),1000);
+
+        Assert.assertEquals(1, testSearchServer.getSearchServer().execute(Search.getById("2"), document).getNumOfResults());
+        Assert.assertEquals(1, testSearchServer.getSearchServer().execute(Search.getById("3"), document).getNumOfResults());
+        Assert.assertEquals(0, testSearchServer.getSearchServer().execute(Search.fulltext(), document).getNumOfResults());
+
+        Thread.sleep(1100);
+
+        Assert.assertEquals(2, testSearchServer.getSearchServer().execute(Search.fulltext(), document).getNumOfResults());
+
+        testSearchServer.getSearchServer().clearIndex();
+    }
+
+    @Test
+    public void testRemoveDocumentWithin() throws InterruptedException {
+        Document doc = document.createDoc("4");
+
+        testSearchServer.getSearchServer().index(doc);
+        testSearchServer.getSearchServer().commit();
+
+        testSearchServer.getSearchServer().deleteWithin(doc, 1000);
+
+        Assert.assertEquals(0, testSearchServer.getSearchServer().execute(Search.getById("4"), document).getNumOfResults());
+        Assert.assertEquals(1, testSearchServer.getSearchServer().execute(Search.fulltext(), document).getNumOfResults());
+
+        Thread.sleep(1100);
+
+        Assert.assertEquals(0, testSearchServer.getSearchServer().execute(Search.fulltext(), document).getNumOfResults());
+
     }
 
 }
