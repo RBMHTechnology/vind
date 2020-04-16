7861f5c9f1780dfac7fcf1dae7746e1041fe414a
Thomas Kurz <thomas.kurz@redlink.co>
2020-04-16 15:29:33 +0200

    fix test by setting correct locale

--- backend/elasticsearch/src/main/java/com/rbmhtechnology/vind/elasticsearch/backend/util/ElasticQueryBuilder.java
+++ backend/elasticsearch/src/main/java/com/rbmhtechnology/vind/elasticsearch/backend/util/ElasticQueryBuilder.java
@@ -41,6 +41,7 @@ import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.SortedSet;
@@ -92,6 +93,7 @@ public class ElasticQueryBuilder {
                                 ScriptType.INLINE,
                                 "painless",
                                 String.format(
+                                        Locale.ENGLISH,
                                         "if(doc['%s'].size()!=0)" +
                                                 "doc['%s'].arcDistance(%f,%f);" +
                                             "else []",
